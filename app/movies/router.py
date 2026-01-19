12a730beef777d8631bb15b1264cb62ebe58dc58
Stepan <stepsyota@gmail.com>
2026-01-19 22:20:07 +0300

    ADD: post /movies endpoint processing

--- app/movies/router.py
+++ app/movies/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from app.movies.services import get_movies_from_db, set_movie_to_db
 from app.database import get_session
+from app.movies.schemas import MovieCreate
 
 
 router = APIRouter(
@@ -15,6 +16,6 @@ async def get_movies(session = Depends(get_session)):
     return movies
 
 @router.post("/")
-async def set_movie(session = Depends(get_session)):
-    movie = await set_movie_to_db(session)
-    return movie
+async def set_movie(movie : MovieCreate, session = Depends(get_session)):
+    created_movie = await set_movie_to_db(movie, session)
+    return created_movie
