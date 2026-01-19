b4bf127e0a8f9d12da27e226f22cd038ea349b9d
Stepan <stepsyota@gmail.com>
2026-01-19 22:20:44 +0300

    ADD: service func to add movie to db

--- app/movies/services.py
+++ app/movies/services.py
@@ -1,8 +1,9 @@
 from sqlalchemy import text
+from app.movies.tables import Film
 
 async def get_movies_from_db(session):
     result = await session.execute(text("SELECT * FROM films"))
-    rows = result.fetchall()
+    rows = result.mappings().all()
     list_movies = []
     for row in rows:
         list_movies.append({
@@ -14,5 +15,13 @@ async def get_movies_from_db(session):
         })
     return list_movies
 
-async def set_movie_to_db(session):
-    pass
+async def set_movie_to_db(movie, session):
+    orm_movie = Film(
+        filmname = movie.filmname,
+        year = movie.year,
+        poster = movie.poster,
+        rating_critics = movie.rating_critics
+    )
+    session.add(orm_movie)
+    await session.flush() # только так появится id фильма
+    return orm_movie
