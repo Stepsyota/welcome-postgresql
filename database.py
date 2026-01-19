09b77cb725e8cac8afcbbc059b5f50477b8818df
Stepan <stepsyota@gmail.com>
2026-01-19 12:37:15 +0300

    ADD: session factory, func get_session for depends

--- database.py
+++ database.py
@@ -1,16 +1,34 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from dotenv import load_dotenv
+from os import getenv
 
-#"postgresql://user:password@postgresserver/db"
-DATABASE_URL = "postgresql://user:password@localhost:5542/db"
 
-engine = create_engine(DATABASE_URL)
+load_dotenv()
 
-try:
-    with engine.connect() as connection:
-        print("Connection succesful!")
-except Exception as e:
-    print(f"Error connection: {e}")
+#"postgresql+asyncpg://user:password@postgresserver/db"
+DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{getenv('POSTGRESQL_USER')}:"
+    f"{getenv('POSTGRESQL_PASSWORD')}@"
+    f"{getenv('POSTGRESQL_HOST')}:"
+    f"{getenv('POSTGRESQL_PORT')}/"
+    f"{getenv('POSTGRESQL_DB')}"
+)
 
-# Создание сессии
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session_factory = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False
+)
+
+async def get_session():
+    session = async_session_factory()
+    try:
+        yield session
+        await session.commit()
+    except:
+        await session.rollback()
+    finally:
+        await session.close()
