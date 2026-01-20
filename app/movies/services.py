c3815cc27a070a868a7088708f7743509e359fb6
Stepan <stepsyota@gmail.com>
2026-01-20 22:47:03 +0300

    ADD: func for get, set, update, delete movie from db

--- app/movies/services.py
+++ app/movies/services.py
@@ -1,8 +1,10 @@
-from sqlalchemy import text
+from sqlalchemy import select, text
 from app.movies.tables import Film
 
 async def get_movies_from_db(session):
-    result = await session.execute(text("SELECT * FROM films"))
+    # RAW SQL
+    """
+    result = await session.execute(text("SELECT id_film, filmname, year, poster, rating_critics FROM films"))
     rows = result.mappings().all()
     list_movies = []
     for row in rows:
@@ -14,6 +16,23 @@ async def get_movies_from_db(session):
             "rating_critics" : row['rating_critics']
         })
     return list_movies
+    """
+    # OR
+    """
+    result = await session.execute(text("SELECT id_film, filmname, year, poster, rating_critics FROM films"))
+    return result.mappings().all()
+    """
+    # ORM
+    query = select(Film)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def get_movie_from_db(id, session):
+    query = select(Film).where(Film.id_film == id)
+    result = await session.execute(query)
+    return result.scalars().one_or_none()
+
 
 async def set_movie_to_db(movie, session):
     orm_movie = Film(
@@ -25,3 +44,25 @@ async def set_movie_to_db(movie, session):
     session.add(orm_movie)
     await session.flush() # только так появится id фильма
     return orm_movie
+
+
+async def update_movie_from_db(id, movie_update, session):
+    query = select(Film).where(Film.id_film == id)
+    result = await session.execute(query)
+    movie = result.scalars().one_or_none()
+
+    update_data = movie_update.model_dump(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(movie, field, value)
+    return movie
+
+
+async def delete_movie_from_db(id, session):
+    query = select(Film).where(Film.id_film == id)
+    result = await session.execute(query)
+    movie = result.scalars().one_or_none()
+    if movie is None:
+        return None
+
+    await session.delete(movie)
+    return movie
