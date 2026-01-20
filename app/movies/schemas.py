b7738ea3f22accd286a61d730c8b6d4fbdd27dc1
Stepan <stepsyota@gmail.com>
2026-01-20 22:46:08 +0300

    ADD: MovieCreate, MovieUpdate, MovieRead schemas

--- app/movies/schemas.py
+++ app/movies/schemas.py
@@ -1,8 +1,19 @@
 from pydantic import BaseModel
 
+class MovieRead(BaseModel):
+    filmname: str
+    year: int | None = None
+    poster: str | None = None
+    rating_critics: float | None = None
 
 class MovieCreate(BaseModel):
     filmname : str
     year : int | None = None
     poster : str | None = None
     rating_critics : float | None = None
+
+class MovieUpdate(BaseModel):
+    filmname: str | None = None
+    year: int | None = None
+    poster: str | None = None
+    rating_critics: float | None = None
